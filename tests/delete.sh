1865f0a14a84f0e0391a3788c8d15a7f49acd8ec
Davide Aimar <davideai@stud.ntnu.no>
2023-01-04 18:15:20 +0100

    fix folder in git

--- tests/delete.sh
+++ tests/delete.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ "$1" = "All" ]
+if [ $# -eq 0 ]
 then 
-find ./output/ -name "*" -type f -delete
+find ./output/ -not -name ".gitkeep" -type f -delete
 echo "All files deleted"
 else
 find ./output/ -name "$1" -type f -delete
-echo "File deleted"
+echo "$1 deleted"
 fi
 echo "Goodbye"
 
